6f8b9d307d3bd2ea292595de6a8dd7fdb017abb5
ne101 <grishastaff1427@gmail.com>
2023-08-11 17:40:28 +0300

    realisation business logic

---
+++ app/src/main/java/com/example/composition/domain/entity/GameSettings.kt
@@ -0,0 +1,10 @@
+package com.example.composition.domain.entity
+
+data class GameSettings (
+    val maxSumValue: Int,
+    val minCountOfRightAnswer: Int,
+    val minPercentOfRightAnswer: Int,
+    val gameTimeInSeconds: Int
+        )
+
+
